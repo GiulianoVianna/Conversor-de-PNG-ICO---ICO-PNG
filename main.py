00d647f4fa4608f8c21cc84b411a17959f09def3
Giuliano Jorge Vianna Silva <giulianoviannasilva@gmail.com>
2023-08-17 20:00:50 -0400

    Atualização main.py

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog
-from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog, QInputDialog
 from PIL import Image
 import os
 
@@ -15,38 +13,44 @@ class ConversorUI(QMainWindow):
         self.bt_ad_origem.clicked.connect(self.selecionar_arquivo)
         self.bt_converter.clicked.connect(self.converter_arquivo)
 
-        # Desativar os botões de maximizar e minimizar
-        self.setWindowFlags(Qt.Window | Qt.WindowCloseButtonHint)
-
         self.show()
 
-    # Função para selecionar um arquivo
+    # Função para selecionar um arquivo de imagem
     def selecionar_arquivo(self):
-        arquivo, _ = QFileDialog.getOpenFileName(self, " Selecionar arquivo", "", "Images (*.png *.ico)")
+        arquivo, _ = QFileDialog.getOpenFileName(self, "Selecionar arquivo", "", "Images (*.png *.ico)")
         if arquivo:
             self.txt_origem.setText(arquivo)
             extensao = os.path.splitext(arquivo)[1][1:].upper()
-            self.lb_status.setText(f'  Arquivo {extensao} selecionado.')
+            self.lb_status.setText(f'Arquivo {extensao} selecionado.')
 
-    # Função para converter arquivo PNG em ICO ou ICO em PNG
+    # Função para converter o arquivo selecionado
     def converter_arquivo(self):
         arquivo_origem = self.txt_origem.text()
         if not arquivo_origem:
-            self.lb_status.setText("  Favor selecionar um arquivo ICO ou PNG.")
+            self.lb_status.setText("Favor selecionar um arquivo ICO ou PNG.")
             return
+
+        # Mostrar janela de seleção de tamanho
+        tamanho, ok = QInputDialog.getItem(self, "Selecionar Tamanho", "Escolha o tamanho do ícone:", ["16x16", "32x32", "64x64", "128x128"], 0, False)
+        if not ok:
+            return
+
+        tamanho = tuple(map(int, tamanho.split('x')))
 
         if arquivo_origem.endswith('.png'):
             arquivo_destino = arquivo_origem[:-4] + '.ico'
         elif arquivo_origem.endswith('.ico'):
             arquivo_destino = arquivo_origem[:-4] + '.png'
         else:
-            self.lb_status.setText("  Formato não suportado.")
+            self.lb_status.setText("Formato não suportado.")
             return
 
         imagem = Image.open(arquivo_origem)
+        if arquivo_destino.endswith('.ico'):
+            imagem = imagem.resize(tamanho)
         imagem.save(arquivo_destino)
 
-        self.lb_status.setText(f"  Salvo como {arquivo_destino}")
+        self.lb_status.setText(f"Arquivo convertido e salvo como {arquivo_destino}")
 
 # Função para iniciar a aplicação
 def iniciar_aplicacao():
